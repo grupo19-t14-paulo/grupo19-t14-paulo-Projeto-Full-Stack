7acf8aac720447b300129426f1fce44ce3aca391
Diego Delli Colli <dellicolli@gmail.com>
2023-06-14 14:04:41 -0300

    create page advertiser

--- front/src/routes/index.tsx
+++ front/src/routes/index.tsx
@@ -3,16 +3,18 @@ import { HomePage } from "../pages/Home";
 import { LoginPage } from "../pages/Login";
 import { RegisterPage } from "../pages/Register";
 import { ProductPage } from "../pages/Product";
+import AdvertiserPage from "../pages/Advertiser";
 
 const RoutesMain = () => {
-    return (
-        <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} />
-            <Route path="/product" element={<ProductPage />} />
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/product" element={<ProductPage />} />
+      <Route path="/advertiser" element={<AdvertiserPage />} />
+    </Routes>
+  );
 };
 
 export { RoutesMain };
