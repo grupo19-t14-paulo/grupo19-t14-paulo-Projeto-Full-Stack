2becd2d2f75e726a0070a6daea17e2600b20f0fe
jessica <jessica_02_06@outlook.com>
2023-06-22 12:46:48 -0300

    feat: create component HeaderProfile | style: mobile component Header

--- front/src/interfaces/ModalInterfaces.tsx
+++ front/src/interfaces/ModalInterfaces.tsx
@@ -2,3 +2,8 @@ export interface IModal {
     modalOpen: boolean
     setModalOpen: boolean | any;
 }
+
+export interface IModalMenuProfile {
+    modalOpenMenuProfile: boolean;
+    setModalOpenMenuProfile: boolean | any;
+}
