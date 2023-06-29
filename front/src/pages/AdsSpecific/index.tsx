a87d195cbaddf1b7961865e7f19bd7dd628e7cd8
Diego Delli Colli <dellicolli@gmail.com>
2023-06-29 20:14:47 -0300

    feat: add tag active or inactive on the advertisers page

--- front/src/pages/AdsSpecific/index.tsx
+++ front/src/pages/AdsSpecific/index.tsx
@@ -35,6 +35,11 @@ const AdsSpecificPage = () => {
     userNameHeader1 ? userNameHeader1[1] : ""
   }`;
 
+  const findPercentage = (price: number, fipePrice: number): number => {
+    const value = ((fipePrice - price) / price) * 100;
+    return Number(value.toFixed(2));
+  };
+
   return (
     <>
       <BackgroundBody>
@@ -74,56 +79,66 @@ const AdsSpecificPage = () => {
           <div className="adsArea">
             <h2 className="advertiserName">Anúncios</h2>
             <section className="sectionCards">
-              {ad?.map((ads) => (
-                <div className="card" key={ads.id}>
-                  <figure>
-                    {ads.images?.map((img, i) =>
-                      i === 0 ? (
-                        <img key={i} src={img.image} alt="Imagem do veículo" />
-                      ) : (
-                        <img key={i} src="" alt="Imagem do veículo" />
-                      )
-                    )}
-                  </figure>
-                  <div className="infoCard">
-                    <h3>
-                      {ads.brand} - {ads.model}
-                    </h3>
-                    <p className="description">{ads.description}</p>
-                    <div className="divNameUserCard">
-                      <span>
-                        <h2 className="initials">
-                          {userNameHeader1 && userNameHeader1.length > 0
-                            ? userNameHeader1[0][0]
-                            : ""}
-                          {userNameHeader1 && userNameHeader1.length > 1
-                            ? userNameHeader1[1]
-                              ? ` ${userNameHeader1[1][0]}`
-                              : ""
-                            : ""}
-                        </h2>
-                      </span>
+              {ad?.map((ads) =>
+                ads.active ? (
+                  <div className="card" key={ads.id}>
+                    {findPercentage(Number(ads.price), Number(ads.value)) >
+                    5 ? (
+                      <span className="dollarSign">{"$"}</span>
+                    ) : null}
+                    <figure>
+                      {ads.images?.map((img, i) =>
+                        i === 0 ? (
+                          <img
+                            key={i}
+                            src={img.image}
+                            alt="Imagem do veículo"
+                          />
+                        ) : (
+                          <img key={i} src="" alt="Imagem do veículo" />
+                        )
+                      )}
+                    </figure>
+                    <div className="infoCard">
                       <h3>
-                        {userNameHeader2 &&
-                          userNameHeader2.replace("undefined", "").trim()}
+                        {ads.brand} - {ads.model}
                       </h3>
-                    </div>
+                      <p className="description">{ads.description}</p>
+                      <div className="divNameUserCard">
+                        <span>
+                          <h2 className="initials">
+                            {userNameHeader1 && userNameHeader1.length > 0
+                              ? userNameHeader1[0][0]
+                              : ""}
+                            {userNameHeader1 && userNameHeader1.length > 1
+                              ? userNameHeader1[1]
+                                ? ` ${userNameHeader1[1][0]}`
+                                : ""
+                              : ""}
+                          </h2>
+                        </span>
+                        <h3>
+                          {userNameHeader2 &&
+                            userNameHeader2.replace("undefined", "").trim()}
+                        </h3>
+                      </div>
 
-                    <div className="divKmPriceYear">
-                      <div className="divKmYear">
-                        <span className="tagInfo">{ads.mileage} KM</span>
-                        <span className="tagInfo">{ads.year}</span>
-                        <p className="price">
-                          {new Intl.NumberFormat("pt-BR", {
-                            style: "currency",
-                            currency: "BRL",
-                          }).format(Number(ads.price))}
-                        </p>
+                      <div className="divKmPriceYear">
+                        <div className="divKmYear">
+                          <span className="tagInfo">{ads.mileage} KM</span>
+                          <span className="tagInfo">{ads.year}</span>
+                          <p className="price">
+                            {new Intl.NumberFormat("pt-BR", {
+                              style: "currency",
+                              currency: "BRL",
+                            }).format(Number(ads.price))}
+                          </p>
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
-              ))}
+                ) : null
+              )}
             </section>
           </div>
         </ContainerAdverts>
